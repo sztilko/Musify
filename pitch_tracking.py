5d94bbbc5ee3e457a81f917ba7f3634eaa5007c4
terbed <terbed@gmail.com>
2019-05-11 23:53:39 +0200

    Play midi note sequnece

--- pitch_tracking.py
+++ pitch_tracking.py
@@ -4,7 +4,7 @@ import sounddevice as sd
 import matplotlib
 matplotlib.use("TKAgg")
 import matplotlib.pyplot as plt
-
+from play_midi import MIDIPlayer
 
 fs = 41000
 
@@ -19,7 +19,23 @@ sd.wait()
 signal = np.squeeze(signal)
 t = np.linspace(0, duration, len(signal))
 
-pitches, magnitudes = librosa.core.piptrack(y=signal, sr=fs, fmin=500, fmax=5000)
+strength = librosa.onset.onset_strength(signal, sr=fs)
+onsets = librosa.onset.onset_detect(signal, sr=fs)
+
+# plt.figure(figsize=(12, 6))
+# plt.plot(signal, label="signal")
+# plt.plot(strength, label="onset strength")
+# for onset in onsets:
+#     plt.axvline(onset)
+# plt.legend(loc="best")
+# plt.show()
+#
+# plt.figure(figsize=(12, 6))
+# plt.plot(strength, label="onset strength")
+# plt.legend()
+# plt.show()
+
+pitches, magnitudes = librosa.core.piptrack(y=signal, sr=fs, fmin=500, fmax=5000,)
 
 print(pitches.shape, magnitudes.shape)
 
@@ -27,13 +43,17 @@ idxs = magnitudes.argmax(axis=0)
 print(f"Shape of idxs: {idxs.shape}")
 
 p = [pitches[idx, t] for t, idx in enumerate(idxs)]
-p = librosa.core.hz_to_note(p)
-p = librosa.core.note_to_midi(p)
+p = librosa.core.hz_to_midi(p)
 
 print(len(p))
 plt.figure(figsize=(12, 6))
 plt.plot(p, '.')
 plt.show()
+
+mp = MIDIPlayer()
+mp.play_midi_sequency(p, duration)
+
+
 
 
 
--- play_midi.py
+++ play_midi.py
@@ -1,22 +1,49 @@
 import pygame
 from pygame import midi
 import time
-pygame.init()
-midi.init()
-
-# list all midi devices
-for x in range(0, pygame.midi.get_count()):
-    print(pygame.midi.get_device_info(x))
 
 
-device = midi.get_default_output_id()
-print(midi.get_default_output_id())
-output = midi.Output(device)
-output.set_instrument(device)
-output.note_on(64, 127)
-time.sleep(2)
-#output.note_off(64)
-output.close()
+class MIDIPlayer:
+    def __init__(self):
+        pygame.init()
+        midi.init()
+        self.device = midi.get_default_output_id()
+        print("Default MIDI device found: " + midi.get_default_output_id())
+
+    @staticmethod
+    def list_all_midi_devices():
+        for x in range(0, pygame.midi.get_count()):
+            print(pygame.midi.get_device_info(x))
+
+    def play_midi_sequency(self, midi_seq, duration, with_instrument=0):
+        """
+        Plays the given MIDI sequence
+
+        :param midi_seq: list of midi notes to play
+        :param duration: the duration of the recording in sec
+        :param with_instrument: the number of the instrument
+        :return: plays the midi notes on given instrument
+        """
+
+        n = len(midi_seq)
+        step = duration/n   # duration of a note
+
+        output = midi.Output(self.device)
+        output.set_instrument(with_instrument)
+
+        for idx, note in enumerate(midi_seq):
+            output.note_on(note, 127)
+            time.sleep(step)
+
+            if idx+1 < n:
+                if midi_seq[idx+1] == note:
+                    pass  # If the next note is the same, keep holding it down
+                else:
+                    output.note_off(note)
+            else:
+                output.note_off(note)
+
+        output.close()
 
 # pygame.mixer.music.load("gf.mid")
 # pygame.mixer.music.play()
