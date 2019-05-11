9c48f3d6e8e96a37f69a3db5d6effb4642942237
Milan Sztilkovics <sztilko@gmail.com>
2019-05-12 01:31:06 +0200

    really simple method for selecting important MIDI notes added handling of zeros in frequencies added

--- pitch_tracking.py
+++ pitch_tracking.py
@@ -4,56 +4,47 @@ import sounddevice as sd
 import matplotlib
 matplotlib.use("TKAgg")
 import matplotlib.pyplot as plt
-from play_midi import MIDIPlayer
+import math
 
 fs = 41000
-
-duration = 5  # seconds
+duration = 3  # seconds
 signal = sd.rec(int(duration * fs), samplerate=fs, channels=1)
 print("Record on...")
 sd.wait()
 sd.play(signal, fs)
 print("Playback...")
 sd.wait()
-
 signal = np.squeeze(signal)
 t = np.linspace(0, duration, len(signal))
+plt.subplot(311)
+plt.plot(t, signal)
 
-strength = librosa.onset.onset_strength(signal, sr=fs)
-onsets = librosa.onset.onset_detect(signal, sr=fs)
+signal_filt = signal**2
+window = 200
+avg_mask = np.ones(window) / window
+signal_filt = np.convolve(signal_filt, avg_mask, 'same')
 
-# plt.figure(figsize=(12, 6))
-# plt.plot(signal, label="signal")
-# plt.plot(strength, label="onset strength")
-# for onset in onsets:
-#     plt.axvline(onset)
-# plt.legend(loc="best")
-# plt.show()
-#
-# plt.figure(figsize=(12, 6))
-# plt.plot(strength, label="onset strength")
-# plt.legend()
-# plt.show()
+plt.subplot(312)
+plt.plot(t, signal_filt)
+th_filt = signal_filt > np.amax(signal_filt)*0.01
+plt.subplot(313)
+plt.plot(t, th_filt)
+plt.show()
 
-pitches, magnitudes = librosa.core.piptrack(y=signal, sr=fs, fmin=500, fmax=5000,)
-
+pitches, magnitudes = librosa.core.piptrack(y=signal, sr=fs, fmin=500, fmax=5000)
 print(pitches.shape, magnitudes.shape)
 
 idxs = magnitudes.argmax(axis=0)
 print(f"Shape of idxs: {idxs.shape}")
-
 p = [pitches[idx, t] for t, idx in enumerate(idxs)]
+p = np.array(p)
+np.place(p, p == 0, 1)  # handle zeros in frequency array (log problem)
 p = librosa.core.hz_to_midi(p)
 
-print(len(p))
 plt.figure(figsize=(12, 6))
-plt.plot(p, '.')
+scale_factor = math.ceil(th_filt.shape[0]/p.shape[0])
+small_th = th_filt[0:-1:scale_factor]
+filtered_midi = small_th*p
+plt.plot(filtered_midi)
+plt.title('Thresholded MIDI notes')
 plt.show()
-
-mp = MIDIPlayer()
-mp.play_midi_sequency(p, duration)
-
-
-
-
-
